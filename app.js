90b60f7b1ef66f5760f2b760c1e231035e32dd95
rahul6373paul <rahul6373paul@github.com>
2020-09-14 20:23:25 +0530

    passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStrore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -40,28 +42,22 @@ app.use(session({
   store: new FileStrore()
 }))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
 
-  if(!req.session.user) {
+  if(!req.user) {
 
       var err = new Error('You are not authorized!');  
-      err.status = 401;
+      err.status = 403;
       return next(err);
 
   } else {
-
-    if (req.session.user === 'authenticated') {
       next();
-    } else {
-      var err = new Error('You are not authorized!');
-      err.status = 403;
-      return next(err);
-    }
-    
   }
   
   
