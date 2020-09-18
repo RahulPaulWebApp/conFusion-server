81d0e1ab5e8ce8ac0d1b8126a029fe5aecefc7de
rahul6373paul <rahul6373paul@github.com>
2020-09-18 22:31:58 +0530

    passport-facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: "337081410865196",
+        clientSecret: "c1463e33c9a954cac516c23a3d755bf8"
+    }
 }
